d3e03a839b8e1cd0c6990a7532adb955c18e3c7e
Mbuyelo Muremela <muremm@unisa.ac.za>
2026-09-06 11:39:18 +0200

    add style project card and grid layout for both mobile and desktop

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,14 @@
 function ProjectCard({ project }) {
   return (
-    <div className="w-[300px]">
-      <div className="w-full">
-        <img src={project.imageURL} alt="project image" />
+    <div className="flex flex-col gap-1 w-[300px] rounded-lg shadow-lg shadow-gray-400 dark:shadow-gray-100 overflow-hidden">
+      <div className="w-full h-[300px] overflow-hidden">
+        <img
+          src={project.imageURL}
+          alt="project image"
+          className="object-cover"
+        />
       </div>
-      <div>
+      <div className="flex flex-col gap1 px-2 py-1 flex-wrap overflow-hidden w-full flex-grow items-center">
         <h3>{project.title}</h3>
         <p>{project.description}</p>
       </div>
--- src/layouts/ProjectsLayout.jsx
+++ src/layouts/ProjectsLayout.jsx
@@ -2,10 +2,16 @@ import ProjectCard from "@/components/ProjectCard";
 
 function ProjectsLayout({ projects }) {
   return (
-    <div>
-      {projects.map((proj) => (
-        <ProjectCard key={proj.id} project={proj} />
-      ))}
+    <div
+      id="projects"
+      className="flex flex-col gap-1 items-center w-full md:max-w-[1100px] mx-auto px-6 py-4 my-15"
+    >
+      <h2>My Projects</h2>
+      <div className="w-full grid justify-items-center md:grid-cols-3 gap-1 flex-grow mt-6">
+        {projects.map((proj) => (
+          <ProjectCard key={proj.id} project={proj} />
+        ))}
+      </div>
     </div>
   );
 }
